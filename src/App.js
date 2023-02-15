86df0d008fdd6d5b6004fa6ebb59035dc5621606
fadyjan <fady.jan95@gmail.com>
2023-02-15 05:02:48 +0200

    Create #7 MainApp Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+import MainImgContainer from "./Components/MainImgContainer/MainImgContainer";
+import TableContainer from "./Components/TableContainer/TableContainer";
+import ClientReviews from "./Components/ClientReviews/ClientReviews";
+import FooterAlbum from "./Components/FooterAlbum/FooterAlbum";
+import FooterBar from "./Components/FooterBar/FooterBar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <NavBar></NavBar>
+        <MainImgContainer></MainImgContainer>
+        <TableContainer></TableContainer>
+        <ClientReviews></ClientReviews>
+        <FooterAlbum></FooterAlbum>
+        <FooterBar></FooterBar>
     </div>
   );
 }
